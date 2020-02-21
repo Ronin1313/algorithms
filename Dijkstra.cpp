7331bb7cacbdc1d8a1c2c2fabd114f8f5d5c7b21
Ronin1313 <61325986+Ronin1313@users.noreply.github.com>
2020-02-21 20:50:20 +0400

    Create Dijkstra.cpp

---
+++ Dijkstra.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>d(100000+1,1e9+1);
+int p[100000+1];
+vector<pair<int,int> >g[100001];
+void Dijkstra(int st){
+	d[st]=0;
+	set<pair<int,int> >s;
+	s.insert({d[st],st});
+	while(!s.empty()){
+		int v=s.begin()->second;
+		s.erase(s.begin());
+		for(int i=0;i<g[v].size();i++){
+			int to=g[v][i].first,len=g[v][i].second;
+			if(d[to]>d[v]+len){
+				s.erase({d[to],to});
+				d[to]=d[v]+len;
+				p[to]=v;
+				s.insert({d[to],to});
+			}
+		}
+	}
+}
+int main(){
+	int n;cin>>n;
+	int m;cin>>m;
+	for(int i=1;i<=m;i++){
+		int u,v;
+		cin>>u>>v;
+		int cost;
+		cin>>cost;
+		g[v].push_back({u,cost});
+		g[u].push_back({v,cost});
+	}
+	int s,f;
+	cin>>s>>f;
+	Dijkstra(s);
+	cout<<d[f]<<endl;
+	int j=f;
+	while(j!=s){
+		cout<<j<<' ';
+		j=p[j];
+	}
+}
